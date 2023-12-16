228924ab429b740a7e9eb4a7fa7e084926d88136
49412 - Vasco Costa <a49412@alunos.isel.pt>
2023-12-16 13:35:32 +0000

    *ADDED* dictionary_lookup_test
    *ADDED* Makefile to build the tests files
    
    *STARTED* Bash script to build all the tests files
    
    *STARTED* Bash script to run all the tests files

---
+++ test/build_all.sh
@@ -0,0 +1,11 @@
+
+# dictionary_create_test
+
+
+# dictionary_add_test
+
+# dictionary_lookup_test
+gcc -g -Wall dictionary_lookup_test.c ../dictionary.c ../invoke/invoke.s -o dictionary_lookup_test -L../bin -lDictionary -I../bin `pkg-config --cflags --libs glib-2.0`
+
+# dictionary_destroy_test
+
---
+++ test/run_all.sh
@@ -0,0 +1,14 @@
+echo "RUNNING dictionary_create_test"
+#./dictionary_create_test
+
+echo ""
+echo "RUNNING dictionary_add_test"
+#./dictionary_add_test
+
+echo ""
+echo "RUNNING dictionary_lookup_test"
+./dictionary_lookup_test
+
+echo ""
+echo "RUNNING dictionary_destroy_test"
+#./dictionary_destroy_test
