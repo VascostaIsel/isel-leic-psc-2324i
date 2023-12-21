d98baa1c9b9bf40fc915ec7e2e6b0644d9578cdc
João Ramos <50462878+JohnnyR2003@users.noreply.github.com>
2023-12-21 16:06:26 +0000

    Unchecked dictionary_create_test.c

---
+++ test/dictionary_create_test.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include "dictionary.h"
+
+
+const char *register_name[] = { "", "rbp", "rbx", "r15", "r14", "r13", "r12" };
+
+int invoke_and_test(void *, void (*)(), int, ...);
+
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
+
+struct {
+    Dictionary *dictionary;
+    const char *word;
+    int expected_result;
+} 
+test_array[] = {
+    { .dictionary = NULL, .word = "test", .expected_result = 1 },
+    { .dictionary = NULL, .word = "test1", .expected_result = 0},
+    { .dictionary = NULL, .word =  NULL, .expected_result = 0 },
+};
+
+Dictionary *dictionary_create();
+
+int main() {
+    Dictionary test_dictionary;
+
+    GHashTable *words = g_hash_table_new(g_str_hash,g_str_equal);
+    g_hash_table_add(words,"test");
+
+    test_array[0].dictionary = &(Dictionary){ .words = words };
+    test_array[1].dictionary = &(Dictionary){ .words = words };
+    test_array[2].dictionary = &(Dictionary){ .words = NULL };
+
+
+
+    for (size_t i = 0; i < ARRAY_SIZE(test_array); i++) {
+        int received_result;
+        int result = invoke_and_test(&test_dictionary, (void (*)())dictionary_create, 0);
+
+        g_hash_table_add(test_dictionary.words,"test");
+
+        int lookup_result = invoke_and_test(&received_result, (void(*)())dictionary_lookup, 2, test_dictionary, test_array[i].word);
+
+        if (lookup_result != 0) {
+            printf("Your function corrupted %s, which is a callee-saved register\n", register_name[result]);
+            break;
+        }
+
+        if(received_result != test_array[i].expected_result){
+            printf("[%zd] Failed - Dictionary or words is NULL\n", i);
+            }
+        else {
+            printf("[%zd] Passed\n", i);
+        }
+
+        // Clean up
+
+            g_hash_table_destroy(test_dictionary.words);
+            g_hash_table_destroy(words);
+    }
+
+    return 0;
+}
