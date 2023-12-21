124c4c5153a974e06d32f38a2dfba28e8a120524
João Ramos <50462878+JohnnyR2003@users.noreply.github.com>
2023-12-21 16:11:53 +0000

    Updated build_all.sh

--- test/build_all.sh
+++ test/build_all.sh
@@ -1,6 +1,6 @@
 
 # dictionary_create_test
-
+gcc -g -Wall dictionary_create_test.c ../dictionary.c ../invoke/invoke.s -o dictionary_create_test -L../bin -lDictionary -I../bin `pkg-config --cflags --libs glib-2.0`
 
 # dictionary_add_test
 gcc -g -Wall dictionary_add_test.c ../dictionary.c ../invoke/invoke.s -o dictionary_add_test -L../bin -lDictionary -I../bin `pkg-config --cflags --libs glib-2.0`
